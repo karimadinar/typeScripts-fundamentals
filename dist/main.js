2b801a90d4d709401d70e69d277bbb2b0544b235
Oussama <saidi.oussama2004@gmail.com>
2025-03-23 13:09:29 +0000

    Adding my TypeScript Project

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vehicle_1 = require("./vehicle");
+const myCar = new vehicle_1.Car("Toyota", "Corolla", 2020);
+myCar.start(); // Logs: "Car engine started"
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { Car } from "./vehicle";
+
+const myCar = new Car("Toyota", "Corolla", 2020);
+myCar.start(); // Logs: "Car engine started"
